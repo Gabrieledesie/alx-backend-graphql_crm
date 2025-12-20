4030057650a060f1d2622c332cdfa24668528bb0
GABRIEL EDESIE <gabrieledesie@gmail.com>
2025-12-20 18:57:48 +0100

    Task 0: Fix Query class to inherit from CRMQuery and graphene.ObjectType

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,11 +1,15 @@
 import graphene
 
 
-class Query(graphene.ObjectType):
+class CRMQuery(graphene.ObjectType):
     hello = graphene.String()
 
     def resolve_hello(self, info):
         return "Hello, GraphQL!"
+
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
 
 
 schema = graphene.Schema(query=Query)
