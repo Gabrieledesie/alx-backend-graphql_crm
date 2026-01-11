931edc4f8d2262b4d87f1eef53edf9cceaee2b24
GABRIEL EDESIE <gabrieledesie@gmail.com>
2026-01-11 16:03:24 +0100

    Fix: Add explicit Product import from crm.models

--- crm/schema.py
+++ crm/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 from django.db import transaction
-from .models import Customer, Product, Order
+from crm.models import Customer, Product, Order
 import re
 
 
